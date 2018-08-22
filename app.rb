a8bccd027b7cbad44391576ce8048b0cb8cd9fb6
Dani Schuhman <dani8439@aol.com>
2018-08-22 16:50:47 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -51,25 +51,25 @@ class App < Sinatra::Base
     when 'divide'
       answer = (@number1 / @number2).to_s
     end
-
-    #old answer: 
-    # operation = params[:operation]
-    # number1 = params[:number1].to_i
-    # number2 = params[:number2].to_i
-    # 
-    # if operation == 'add'
-    #   answer = number1 + number2
-    # elsif operation == 'subtract'
-    #   answer = number1 - number2
-    # elsif operation == 'multiply'
-    #   answer = number1 * number2
-    # elsif operation == 'divide'
-    #   answer = number1 / number2
-    # else
-    #   answer = "Unable to perform this operation"
-    # end
-    # answer.to_s
-    # end
   end
 
 end
+
+#old answer:
+# operation = params[:operation]
+# number1 = params[:number1].to_i
+# number2 = params[:number2].to_i
+#
+# if operation == 'add'
+#   answer = number1 + number2
+# elsif operation == 'subtract'
+#   answer = number1 - number2
+# elsif operation == 'multiply'
+#   answer = number1 * number2
+# elsif operation == 'divide'
+#   answer = number1 / number2
+# else
+#   answer = "Unable to perform this operation"
+# end
+# answer.to_s
+# end
