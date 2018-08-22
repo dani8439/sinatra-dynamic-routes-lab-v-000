1022385105f289b6aed1105ae73514b1773b9e50
Dani Schuhman <dani8439@aol.com>
2018-08-22 16:31:10 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -17,7 +17,7 @@ class App < Sinatra::Base
     @number = params[:number].to_i
     @phrase = params[:phrase]
     @what = @number * @phrase
-    "#{@number}*#{@phrase}"
+    "#{@what}"
   end
 
 end
