361ed2a39df37871259bda2bbe63f99992557c43
Dani Schuhman <dani8439@aol.com>
2018-08-22 16:37:22 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -29,7 +29,9 @@ class App < Sinatra::Base
   get '/say/:word1/:word2/:word3/:word4/:word5' do
     @string = ''
     params.each.with_index(1) do |word, i|
-      @string += params[:wordi] + ''
+      @string += params[:wordi] + ' '
+      @string
+    end
 
   end
 
