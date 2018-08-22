43d8e5a75f33f74137eaaecce2d7be599f2b2b52
Dani Schuhman <dani8439@aol.com>
2018-08-22 16:47:50 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -39,13 +39,15 @@ class App < Sinatra::Base
     @number1 = params[:number1].to_i
     @number2 = params[:number2].to_i
 
+    @answer = ""
+
     case params[:operation]
     when 'add'
       @number1 + @number2
     when 'subtract'
-      @number1 - @number2 
+      @number1 - @number2
     when 'multiply'
-      @number1 * @number2 
+      @number1 * @number2
     when 'divide'
       @number1 / @number2
     end
